4d214d18202999ef798377b873c77298f1d3f240
Ayman <you@example.com>
2022-07-27 17:23:50 +0200

    *

--- edit_payment_method_sales/models/models.py
+++ edit_payment_method_sales/models/models.py
@@ -22,13 +22,22 @@ class EditSaleOrder(models.Model):
 
 
     payment_type_method = fields.Selection(string="Payment Method", selection=[('cod', 'COD'), ('cc', 'CC'), ], required=False, )
-    payment_state = fields.Selection(string="State", selection=[('delivered', 'Delivered'), ('returned', 'Returned'), ], required=False, )
+    payment_state = fields.Selection(string="State", selection=[('delivered', 'Delivered'), ('returned', 'Returned'), ], required=True, )
     is_cash = fields.Boolean(string="Cash",)
     is_visa = fields.Boolean(string="Visa",)
-    visa_commission = fields.Float(string="Visa Commission",  required=False, )
+    visa_commission = fields.Float(string="Visa Amount",  required=False, )
     is_mada = fields.Boolean(string="Mada",)
-    mada_commission = fields.Float(string="Mada Commission",  required=False, )
-    analytic_account_id = fields.Many2one('account.analytic.account', string='Delivery Person')
+    mada_commission = fields.Float(string="Mada Amount",  required=False, )
+    analytic_account_id = fields.Many2one('account.analytic.account', string='Driver Name')
+    awb_no = fields.Char(string="AWB NO", required=False, )
+    origin = fields.Char(string="ORIGIN", required=False, )
+    description = fields.Char(string="DESTINATION", required=False, )
+    receiver_phone = fields.Char(string="RECEIVER PHONE", required=False, )
+    receiver_name = fields.Char(string="RECEIVER Name", required=False, )
+    receiver_address = fields.Char(string="RECEIVER ADDRESS", required=False, )
+    receiver_date = fields.Date(string="RECEIVER DATE", required=False, )
+    on_pieces = fields.Char(string="ON PIECES", required=False, )
+
 
 
     def compute_payment_method(self):
@@ -38,6 +47,9 @@ class EditSaleOrder(models.Model):
             cod_cash=[]
             cod_mada=[]
             cod_visa=[]
+            return_list=[]
+            product_return_sitting = self.env['product.product'].search(
+                [('id', '=', int(self.env['ir.config_parameter'].sudo().get_param('product_return_id', )))])
             product_delivery_sitting = self.env['product.product'].search(
                 [('id', '=', int(self.env['ir.config_parameter'].sudo().get_param('product_delivery_id', )))])
             collect_on_delivery_sitting = self.env['product.product'].search([('id', '=', int(self.env['ir.config_parameter'].sudo().get_param('collect_on_delivery_id', )))])
@@ -46,56 +58,69 @@ class EditSaleOrder(models.Model):
             product_visa_sitting = self.env['product.product'].search(
                 [('id', '=', int(self.env['ir.config_parameter'].sudo().get_param('product_visa_id', )))])
             visa_commission_percent_sitting = float(self.env['ir.config_parameter'].sudo().get_param('visa_commission_percent', ))
-            if rec.payment_type_method == 'cc' :
-                print('rec.payment_type_method',rec.payment_type_method)
-                cc_list.append([0, 0, {
-                    'product_id': product_delivery_sitting.id,
-                    'name': product_delivery_sitting.name,
-                    'analytic_account_id': rec.analytic_account_id.id,
-                    'product_uom_qty': 1 ,
-                    'price_unit': 14,
-                }])
-                rec.order_line = cc_list
-            elif rec.payment_type_method == 'cod' :
-
-                if rec.is_cash:
-                    print('is_cashis_cashis_cash')
-                    cod_cash.append([0, 0, {
+            rec.order_line = False
+            if rec.payment_state == 'delivered':
+                if rec.payment_type_method == 'cc':
+                    print('rec.payment_type_method', rec.payment_type_method)
+                    cc_list.append([0, 0, {
                         'product_id': product_delivery_sitting.id,
                         'name': product_delivery_sitting.name,
                         'analytic_account_id': rec.analytic_account_id.id,
                         'product_uom_qty': 1,
                         'price_unit': 14,
                     }])
-                    cod_cash.append([0, 0, {
-                        'product_id': collect_on_delivery_sitting.id,
-                        'name': collect_on_delivery_sitting.name,
-                        'analytic_account_id': rec.analytic_account_id.id,
-                        'product_uom_qty': 1,
-                        'price_unit': 4,
-                    }])
-                    print('cod_cashcod_cash',cod_cash)
-                    rec.order_line = cod_cash
-                if rec.is_mada:
-                    cod_mada.append([0, 0, {
-                        'product_id': product_mada_sitting.id,
-                        'name': product_mada_sitting.name,
-                        'analytic_account_id': rec.analytic_account_id.id,
-                        'product_uom_qty': 1,
-                        'price_unit': mada_commission_percent_sitting / 100 * rec.mada_commission ,
-                    }])
-                    rec.order_line = cod_mada
-                if rec.is_visa:
-                    print('tttttttttttttttt mada')
-                    cod_visa.append([0, 0, {
-                        'product_id': product_visa_sitting.id,
-                        'name': product_visa_sitting.name,
-                        'analytic_account_id': rec.analytic_account_id.id,
-                        'product_uom_qty': 1,
-                        'price_unit': visa_commission_percent_sitting / 100 * rec.visa_commission ,
-                    }])
-                    print('cod_visa',cod_visa)
-                    rec.order_line = cod_visa
+                    rec.order_line = cc_list
+                elif rec.payment_type_method == 'cod':
+
+                    if rec.is_cash:
+                        print('is_cashis_cashis_cash')
+                        cod_cash.append([0, 0, {
+                            'product_id': product_delivery_sitting.id,
+                            'name': product_delivery_sitting.name,
+                            'analytic_account_id': rec.analytic_account_id.id,
+                            'product_uom_qty': 1,
+                            'price_unit': 14,
+                        }])
+                        cod_cash.append([0, 0, {
+                            'product_id': collect_on_delivery_sitting.id,
+                            'name': collect_on_delivery_sitting.name,
+                            'analytic_account_id': rec.analytic_account_id.id,
+                            'product_uom_qty': 1,
+                            'price_unit': 4,
+                        }])
+                        print('cod_cashcod_cash', cod_cash)
+                        rec.order_line = cod_cash
+                    if rec.is_mada:
+                        cod_mada.append([0, 0, {
+                            'product_id': product_mada_sitting.id,
+                            'name': product_mada_sitting.name,
+                            'analytic_account_id': rec.analytic_account_id.id,
+                            'product_uom_qty': 1,
+                            'price_unit': mada_commission_percent_sitting / 100 * rec.mada_commission,
+                        }])
+                        rec.order_line = cod_mada
+                    if rec.is_visa:
+                        print('tttttttttttttttt mada')
+                        cod_visa.append([0, 0, {
+                            'product_id': product_visa_sitting.id,
+                            'name': product_visa_sitting.name,
+                            'analytic_account_id': rec.analytic_account_id.id,
+                            'product_uom_qty': 1,
+                            'price_unit': visa_commission_percent_sitting / 100 * rec.visa_commission,
+                        }])
+                        print('cod_visa', cod_visa)
+                        rec.order_line = cod_visa
+            elif rec.payment_state == 'returned':
+                return_list.append([0, 0, {
+                    'product_id': product_return_sitting.id,
+                    'name': product_return_sitting.name,
+                    'analytic_account_id': rec.analytic_account_id.id,
+                    'product_uom_qty': 1,
+                    'price_unit': product_return_sitting.list_price,
+                }])
+                print('return_listreturn_list',return_list)
+                rec.order_line = return_list
+            rec.action_confirm()
 
 
 
@@ -113,6 +138,7 @@ class sale_payment_config_settings(models.TransientModel):
     mada_commission_percent = fields.Float(string="Mada Commission Percent %",  required=False, )
     product_visa_id = fields.Many2one(comodel_name="product.product", string="Product Visa Commission", required=False, )
     visa_commission_percent = fields.Float(string="Visa Commission Percent %",  required=False, )
+    product_return_id = fields.Many2one(comodel_name="product.product", string="Product Return", required=False, )
 
 
 
@@ -132,7 +158,8 @@ class sale_payment_config_settings(models.TransientModel):
             product_mada_id=int(self.env['ir.config_parameter'].sudo().get_param('product_mada_id',)),
             mada_commission_percent=(self.env['ir.config_parameter'].sudo().get_param('mada_commission_percent', )),
             product_visa_id=int(self.env['ir.config_parameter'].sudo().get_param('product_visa_id', )),
-            visa_commission_percent=(self.env['ir.config_parameter'].sudo().get_param('visa_commission_percent', ))
+            visa_commission_percent=(self.env['ir.config_parameter'].sudo().get_param('visa_commission_percent', )),
+            product_return_id=int(self.env['ir.config_parameter'].sudo().get_param('product_return_id', ))
         )
         return res
 
@@ -144,4 +171,5 @@ class sale_payment_config_settings(models.TransientModel):
         self.env['ir.config_parameter'].sudo().set_param('mada_commission_percent', self.mada_commission_percent)
         self.env['ir.config_parameter'].sudo().set_param('product_visa_id', self.product_visa_id.id)
         self.env['ir.config_parameter'].sudo().set_param('visa_commission_percent', self.visa_commission_percent)
+        self.env['ir.config_parameter'].sudo().set_param('product_return_id', self.product_return_id.id)
 
